3d677c859ed1c44b6ee783b350de2b1c6353fb3e
krissyjames <kristina.ashley.cook@gmail.com>
2024-12-10 21:00:25 -0500

    refactors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import axios from "axios";
 
 class App extends React.Component {
  constructor(props) {
@@ -20,79 +21,102 @@ class App extends React.Component {
   }
   }
  
-componentDidMount() {
-  this.getCoordinatesWithFetch();
-}
+async componentDidMount() {
+  const test = await this.getCoordinatesWithAxios();
+  const weath = await this.get3DayWeatherWithAxios();
+  console.log("ressie", test)
+this.setState({
+  lat: res.data[0].lat, 
+  lon: res.data[0].lon,
+  temp: Math.floor(weath.data.list[0].main.temp),
+  // date: day,
+  weather: weath.data.list[0].weather[0].main,
+  temp2: Math.floor(weath.data.list[8].main.temp),
+  // date2: day2,
+  weather2: weath.data.list[8].weather[0].main,
+  temp3: Math.floor(weath.data.list[16].main.temp),
+  // date3: day3,
+  weather3: weath.data.list[16].weather[0].main, 
+})
+console.log("statehere", this.state);
+  }
 
 
-getCoordinatesWithFetch = async () => {
+
+getCoordinatesWithAxios = async () => {
   let apiKey = process.env.REACT_APP_WEATHER_API_KEY;
   let city = "Canton";
   let stateCode = "Michigan";
   let countryCode = "US";
   let url = "http://api.openweathermap.org/geo/1.0/direct";
 
-  try {
-    let res = await fetch(`${url}?q=${city},${stateCode},${countryCode}&appid=${apiKey}`);
-    console.log("res", res);
-    if (!res.ok) {
-      throw new Error(`Error: ${res.status} - ${res.statusText}`)
-    }
-    let data= await res.json();
-    console.log("data", data);
-   this.setState({ 
-    lat: data[0].lat, 
-    lon: data[0].lon 
-  })
 
-  } catch (error) {
-    console.log(error.message);
-
-  }
-  this.get3DayWeatherWithFetch();
+try {
+  let res = await axios.get(`${url}?q=${city},${stateCode},${countryCode}&appid=${apiKey}`);
+  if (res.statusText != "OK") {
+    throw new Error(`Error: ${res.status} - ${res.statusText}`)
+  } else if (res.statusText === "OK") {
+     let newState = await this.setState({ 
+      lat: res.data[0].lat, 
+      lon: res.data[0].lon 
+    })
+    console.log("new state", newState);
+    return this.get3DayWeatherWithAxios(res.data[0].lat, res.data[0].lon);
+  }}
+  
+catch(error){
+  console.log(error.message)
 }
-get3DayWeatherWithFetch = async () => {
+}
+get3DayWeatherWithAxios = async (lat, lon) => {
+  console.log("state again", this.state);
+  console.log("weather firing");
+  if ((lon === null) || (lat === null)) {
+    return console.log("state null");
+  }
   let apiKey = process.env.REACT_APP_WEATHER_API_KEY;
   let url = "http://api.openweathermap.org/data/2.5/forecast";
-  try {
-    let res = await fetch(`${url}?lat=${this.state.lat}&lon=${this.state.lon}&units=imperial&appid=${apiKey}`);
+  console.log(lat);
+  let res = axios.get(`${url}?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
+  res.then (res => {
     console.log("new res", res);
-    if (!res.ok) {
-      throw new Error(`Error: ${res.status} - ${res.statusText}`)
-    }
-    let data= await res.json();
-    console.log("help", data);
-    let timestamp = data.list[0].dt_txt;
-    let parsedDate = Date.parse(timestamp);
-    let date = new Date(parsedDate);
-    let day = date.toLocaleDateString('en-us', {weekday:"short", month:"long", day:"numeric" });
-    let timestamp2 = data.list[8].dt_txt;
-    let parsedDate2 = Date.parse(timestamp2);
-    let date2 = new Date(parsedDate2);
-    let day2 = date2.toLocaleDateString('en-us', {weekday:"short", month:"long", day:"numeric" });
-    let timestamp3 = data.list[16].dt_txt;
-    let parsedDate3 = Date.parse(timestamp3);
-    let date3 = new Date(parsedDate3);
-    let day3 = date3.toLocaleDateString('en-us', {weekday:"short", month:"long", day:"numeric" });
-    // console.log(day);
-
-    this.setState({
-      temp: Math.floor(data.list[0].main.temp),
-      date: day,
-      weather: data.list[0].weather[0].main,
-      temp2: Math.floor(data.list[8].main.temp),
-      date2: day2,
-      weather2: data.list[8].weather[0].main,
-      temp3: Math.floor(data.list[16].main.temp),
-      date3: day3,
-      weather3: data.list[16].weather[0].main,
-    })
-  } catch (error) {
-    console.log("error", error.message);
-
-  }
+    console.log("status", res.statusText )
   
+    if (res.statusText != "OK") {
+      throw new Error(`Error: ${res.status} - ${res.statusText}`)
+    } else {
+        let timestamp = res.data.list[0].dt_txt;
+        let parsedDate = Date.parse(timestamp);
+        let date = new Date(parsedDate);
+        let day = date.toLocaleDateString('en-us', {weekday:"short", month:"long", day:"numeric" });
+        let timestamp2 = res.data.list[8].dt_txt;
+        let parsedDate2 = Date.parse(timestamp2);
+        let date2 = new Date(parsedDate2);
+        let day2 = date2.toLocaleDateString('en-us', {weekday:"short", month:"long", day:"numeric" });
+        let timestamp3 = res.data.list[16].dt_txt;
+        let parsedDate3 = Date.parse(timestamp3);
+        let date3 = new Date(parsedDate3);
+        let day3 = date3.toLocaleDateString('en-us', {weekday:"short", month:"long", day:"numeric" });
+        this.setState({
+          temp: Math.floor(res.data.list[0].main.temp),
+          date: day,
+          weather: res.data.list[0].weather[0].main,
+          temp2: Math.floor(res.data.list[8].main.temp),
+          date2: day2,
+          weather2: res.data.list[8].weather[0].main,
+          temp3: Math.floor(res.data.list[16].main.temp),
+          date3: day3,
+          weather3: res.data.list[16].weather[0].main,
+        });
+console.log("check2", res.data);
+  }}
+)
+res.catch(error => {
+  console.log(error.message)})
+
 }
+  
+
 
 
   render() {
